054430afd2ad9ce88494292f26417f52b1c85c16
Carlos Bolívar <cebemind@hotmail.com>
2026-05-26 21:53:33 -0400

    feat(types): define PullRequest payload structs

--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,11 @@ pub struct User {
 }
 
 #[derive(Debug, Clone, Deserialize)]
+pub struct Label {
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct Installation {
     pub id: u64,
 }
@@ -17,5 +22,34 @@ pub struct Repository {
     pub full_name: String,
 }
 
+#[derive(Debug, Clone, Deserialize)]
+pub struct PullRequest {
+    pub number: u64,
+    pub title: Option<String>,
+    pub body: Option<String>,
+    pub merged: Option<bool>,
+    pub additions: Option<u32>,
+    pub deletions: Option<u32>,
+    pub user: User,
+    pub labels: Vec<Label>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct PullRequestEvent {
+    pub action: String,
+    pub pull_request: PullRequest,
+    pub repository: Repository,
+    pub sender: User,
+    pub installation: Option<Installation>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ChangedFile {
+    pub filename: String,
+    pub status: String,
+}
+
 #[derive(Debug, Clone)]
-pub enum WebhookEvent {}
+pub enum WebhookEvent {
+    PullRequest(PullRequestEvent),
+}
